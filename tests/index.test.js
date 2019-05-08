56dfbd051a5fb459b705d950f83de8a9b0748ac4
Mark Waite <mwaite@cloudbees.com>
2019-05-08 17:24:29 -0600

    Add comma to conclude each test

--- tests/index.test.js
+++ tests/index.test.js
@@ -8,14 +8,14 @@ test('Check greeting in header', async t => {
 
    await t
        .expect(messageHeader.innerText).contains('Hello', 'message header contains text "Hello"');
-}
+},
 
 test('Check closing in header', async t => {
    const messageHeader = Selector('#message');
 
    await t
        .expect(messageHeader.innerText).contains('World', 'message header contains text "World"');
-}
+},
 
 test('Check message header', async t => {
    const messageHeader = Selector('#message');
