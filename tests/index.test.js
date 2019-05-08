56b22ebc498cf1151a8b30b76102e5b836853e51
Mark Waite <mwaite@cloudbees.com>
2019-05-08 17:22:57 -0600

    Add two tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,6 +3,20 @@ import { Selector } from 'testcafe';
 fixture `Initial page`
    .page `localhost:8080`;
 
+test('Check greeting in header', async t => {
+   const messageHeader = Selector('#message');
+
+   await t
+       .expect(messageHeader.innerText).contains('Hello', 'message header contains text "Hello"');
+}
+
+test('Check closing in header', async t => {
+   const messageHeader = Selector('#message');
+
+   await t
+       .expect(messageHeader.innerText).contains('World', 'message header contains text "World"');
+}
+
 test('Check message header', async t => {
    const messageHeader = Selector('#message');
 
